7227250f23539892c12f82d110225284594ab86a
aakashpsl <aakash_chaudhary@persistent.com>
2024-02-21 13:21:19 +0000

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       {
         isSignedIn ? (
           <>
-            <UserProfile only="account" />
+            
             <SignOutButton signOutCallback={() => navigate('/sign-in')} />
           </>
         ) : (
